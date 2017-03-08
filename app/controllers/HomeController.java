598afaeb8f74a5622e504c6d66f08ffc9e27fcf9
AurecchiaP <paolo@paolos-mbp.home>
2017-03-08 22:45:26 +0100

    Repo download 0.1

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import com.google.gson.Gson;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.routing.JavaScriptReverseRouter;
@@ -8,6 +10,8 @@ import play.twirl.api.Content;
 import utils.BasicParser;
 import utils.JavaPackage;
 import utils.CubePacking;
+
+import java.io.File;
 
 public class HomeController extends Controller {
 
@@ -23,9 +27,38 @@ public class HomeController extends Controller {
         return ok(views.html.index.render());
     }
 
-    public Result getClasses() {
+    void deleteDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                deleteDir(f);
+            }
+        }
+        file.delete();
+    }
 
-        JavaPackage pkg = BasicParser.parseRepo("/Users/paolo/Documents/6th semester/thesis/commons-math");
+    public Result getClasses() {
+        // FIXME do this properly; should there be option to authenticate and use private repos?though then they
+        // FIXME would be on the server
+
+        boolean web = false;
+        JavaPackage pkg;
+        if(web) {
+            deleteDir(new File("/Users/paolo/Documents/6th semester/thesis/webcity/repository"));
+            try {
+                Git git = Git.cloneRepository()
+                        .setURI("https://github.com/junit-team/junit4.git")
+                        .setDirectory(new File("/Users/paolo/Documents/6th semester/thesis/webcity/repository"))
+                        .call();
+            } catch (GitAPIException e) {
+                System.out.println("failed to download repo");
+                e.printStackTrace();
+            }
+
+            pkg = BasicParser.parseRepo("/Users/paolo/Documents/6th semester/thesis/webcity/repository");
+        }
+        else{
+        pkg = BasicParser.parseRepo("/Users/paolo/Documents/6th semester/thesis/commons-math");
 
         Gson gson = new Gson();
         new CubePacking(pkg);
--- build.sbt
+++ build.sbt
@@ -13,4 +13,6 @@ libraryDependencies += filters
 
 libraryDependencies += "com.google.code.gson" % "gson" % "2.8.0"
 libraryDependencies += "com.github.javaparser" % "javaparser-core" % "3.1.0"
+libraryDependencies += "org.eclipse.jgit" % "org.eclipse.jgit" % "4.6.0.201612231935-r"
+
 
