4e8e369bf863fba184c10ac7cb55d220e280c9f2
AurecchiaP <paolo@paolos-mbp.home>
2017-03-10 16:47:31 +0100

    Added repo check

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,13 @@
 package controllers;
 
 import com.google.gson.Gson;
+import com.google.inject.Inject;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LsRemoteCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import play.Logger;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.routing.JavaScriptReverseRouter;
@@ -11,8 +16,15 @@ import models.JavaPackage;
 import utils.CubePacking;
 
 import java.io.File;
+import java.util.List;
 
 public class HomeController extends Controller {
+
+    private String currentRepo;
+
+    @Inject
+    FormFactory formFactory;
+
 
     /**
      * Used by Play Framework to create routes callable from Javascript.
@@ -50,13 +62,15 @@ public class HomeController extends Controller {
         // FIXME do this properly; should there be option to authenticate and use private repos?though then they
         // FIXME would be on the server
 
+//        https://github.com/junit-team/junit4.git"
+
         boolean web = false;
         JavaPackage pkg;
         if(web) {
             deleteDir(new File("/Users/paolo/Documents/6th semester/thesis/webcity/repository"));
             try {
                 Git git = Git.cloneRepository()
-                        .setURI("https://github.com/junit-team/junit4.git")
+                        .setURI(currentRepo)
                         .setDirectory(new File("/Users/paolo/Documents/6th semester/thesis/webcity/repository"))
                         .call();
             } catch (GitAPIException e) {
@@ -83,7 +97,22 @@ public class HomeController extends Controller {
      * Route for the visualisation page; not yet used.
      */
     public Result visualization() {
-        System.out.println("in vis");
-        return ok(views.html.index2.render());
+
+        DynamicForm dynamicForm = formFactory.form().bindFromRequest();
+
+        String formRepo = dynamicForm.get("repo");
+
+        final LsRemoteCommand lsCmd = new LsRemoteCommand(null);
+            lsCmd.setRemote(formRepo);
+        try {
+            System.out.println(lsCmd.call().toString());
+            System.out.println("valid repo");
+            currentRepo = formRepo;
+            return ok(views.html.index2.render());
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            System.out.println("invalid repo");
+            return badRequest();
+        }
     }
 }
